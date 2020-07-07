d3d7618debb26109dd5e7e0a89fc9e3351b32835
hui1601 <hui1601@naver.com>
2020-07-07 22:36:50 +0900

    Update Hibot.js

--- Hibot.js
+++ Hibot.js
@@ -3,7 +3,7 @@ Project - M
 5th Open Source : Moka
 © 2018 Dark Tornado, All rights reserved.
 <작동 방식>
-1. 채팅방에서 사람들이 하는 채팅을 '내장메모리/Hibot/' 폴더에 '방이름.txt' 파일로 저장.
+1. 채팅방에서 사람들이 하는 채팅을 '내장메모리/Hina/' 폴더에 '방이름.txt' 파일로 저장.
   -> 각 채팅들은 엔터로 구분합니다. 따라서, 엔터가 포함된 채팅은 저장하지 않습니다.
   -> 학습 및 채팅 전송은 채팅방마다 따로따로 작동합니다.
 2. 해당 채팅방에서 채팅이 수신되면 10% 확률로 수신된 채팅과 파일 안에 있는 채팅의 유사도를 대충 검사.
@@ -37,7 +37,7 @@ const levenshtein = require('levenshtein');
 const sdcard = android.os.Environment.getExternalStorageDirectory().getAbsolutePath(); //내장메모리 최상위 경로
 
 /*상수 (객체) 선언*/
-const Hibot = {}; //Hibot 관련 객체
+const Hina = {}; //Hina 관련 객체
 const DB = {}; //파일 입/출력용 객제인데, 이름이 DB인건 기분탓
 const preChat = {}; //도배 방지 구현용
 const lastSender = {}; //보낸 사람 구분용
@@ -72,16 +72,16 @@ function randInt(min, max) {
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-/*Hibot 객체*/
-Hibot.checkWord = function(que, msg) { //적당히 비슷한 말인지 비교
+/*Hina 객체*/
+Hina.checkWord = function(que, msg) { //적당히 비슷한 말인지 비교
 	return levenshtein.get(toKorChars(que).join(), toKorChars(msg).join());
 };
-Hibot.getReply = function(room, msg, data) { //수신된 채팅에 대한 적당한 답변 반환
+Hina.getReply = function(room, msg, data) { //수신된 채팅에 대한 적당한 답변 반환
 	if (data != null) { //저장된 채팅이 없으면 작동 안함
 		var result = []; //비슷한 말들이 들어갈 배열
 		var min = 10000; //최대 유사도 값
 		for (var n = 0; n < data.length - 1; n++) { //저장된 채팅들 중 비슷하다 싶은 녀석들을 배열에 넣을건데,
-			var count = Hibot.checkWord(data[n], msg); //유사도(?)를 가져와서
+			var count = Hina.checkWord(data[n], msg); //유사도(?)를 가져와서
 			if (count < min) { //기존에 확인했던 녀석들보다 유사도가 낮으면, 결과 배열 초기화 및 최소 유사도 값 변경
 				min = count;
 				result = [];
@@ -94,15 +94,15 @@ Hibot.getReply = function(room, msg, data) { //수신된 채팅에 대한 적당
 	}
 	return null; //일치하는게 없거나, 저장된 채팅이 없거나, 발동할 확률(?)이 아니면, null 반환
 };
-Hibot.say = function(msg, replier) { //그냥 말하는 함수
+Hina.say = function(msg, replier) { //그냥 말하는 함수
 	replier.reply(msg); //앞에다가 이상한 문구 붙이는 용도
 };
-Hibot.isValidData = function(msg) { //배울 만한 채팅인지
-	var invalids = ["/", "lc ", "c", "샵검색: #", "파일: ", "이모티콘을 보냈습니다.", "사진을 보냈습니다.", "동영상을 보냈습니다."];
+Hina.isValidData = function(msg) { //배울 만한 채팅인지
+	var invalids = ["/", "lc ", "c", "샵검색: #", "파일: "];
 	for (var n = 0; n < invalids.length; n++) {
-		if (msg.charAt(0) == invalids[n]) return false; //특정 문자로 시작하는 것은 학습 X.	
+		if (msg.startsWith(invalids[n])) return false; //특정 문자로 시작하는 것은 학습 X.	
 	}
-	var noStudy = ["\'", "\"", "[", "]", "{", "}", "⎼", ".com", ".tk", ".org", ".kr", ".net", ".xxx", ".gov", "<", ">", "_", "҉", "\\"];
+	var noStudy = ["\'", "\"", "[", "]", "{", "}", "⎼", ".com", ".tk", ".org", ".kr", ".net", ".xxx", ".gov", "<", ">", "_", "҉", "\\", " 보냈습니다."];
 	for (var n = 0; n < noStudy.length; n++) {
 		if (msg.indexOf(noStudy[n]) != -1) return false;
 	}
@@ -111,23 +111,23 @@ Hibot.isValidData = function(msg) { //배울 만한 채팅인지
 
 /*DB 객체*/
 DB.createDir = function() { //배운 채팅들이 저장될 폴더를 만드는 함수
-	var folder = new java.io.File(sdcard + "/Hibot/"); //File 인스턴스 생성
+	var folder = new java.io.File(sdcard + "/Hina/"); //File 인스턴스 생성
 	folder.mkdirs(); //폴더 생성
 };
 DB.saveData = function(name, msg) { //파일에 내용을 저장하는 함수
 	try { //사실, 나도 어디서 긁어와서 이곳저곳에서 사용하는 거임
-		var file = new java.io.File(sdcard + "/Hibot/" + name + ".txt");
+		var file = new java.io.File(sdcard + "/Hina/" + name + ".txt");
 		var fos = new java.io.BufferedWriter(new java.io.OutputStreamWriter(new java.io.FileOutputStream(file), java.nio.charset.StandardCharsets.UTF_8));
 		var str = new java.lang.String(msg);
 		fos.write(msg);
 		fos.close();
 	} catch (e) {
-		Log.debug(e + ", " + e.lineNumber);
+		Log.debug(e.lineNumber + ": " + e);
 	}
 };
 DB.readData = function(name) { //파일에 저장된 내용을 불러오는 함수
 	try { //사실, 나도 어디서 긁어와서 이곳저곳에서 사용하는 거임
-		var file = new java.io.File(sdcard + "/Hibot/" + name + ".txt");
+		var file = new java.io.File(sdcard + "/Hina/" + name + ".txt");
 		if (!file.exists()) return null;
 		var fis = new java.io.FileInputStream(file);
 		var isr = new java.io.InputStreamReader(fis, java.nio.charset.StandardCharsets.UTF_8);
@@ -172,9 +172,15 @@ function response(room, msg, sender, isGroupChat, replier) {
 	if (!isGroupChat) {
 		return;
 	}
+	/*적당한 채팅 하나 가져와서 답장(?)하는 부분*/
+	if (randInt(1, 20) == 1) { //5% 확률로 작동
+		var data = DB.readData(room); //저장된 채팅들을 불러옴
+		var chat = Hina.getReply(room, msg, data.split("\n")); //적당한거 하나 가져와서
+		if (chat != null) Hina.say(chat, replier); //전송
+	}
 
 	/*채팅을 학습하는 부분*/
-	if (Hibot.isValidData(msg)) { //배울 만한 채팅인 경우,
+	if (Hina.isValidData(msg)) { //배울 만한 채팅인 경우,
 		var data = DB.readData(room); //배운 채팅 목록을 가져옴
 		if (data == null) { //아직 배운게 없다면,
 			DB.saveData(room, msg); //새로 저장
@@ -186,12 +192,6 @@ function response(room, msg, sender, isGroupChat, replier) {
 			}
 		}
 	}
-	/*적당한 채팅 하나 가져와서 답장(?)하는 부분*/
-	if (randInt(1, 20) == 1) { //5% 확률로 작동
-		var data = DB.readData(room); //저장된 채팅들을 불러옴
-		var chat = Hibot.getReply(room, msg, data.split("\n")); //적당한거 하나 가져와서
-		if (chat != null) Hibot.say(chat, replier); //전송
-	}
 
 	lastSender[room] = sender;
 }
@@ -200,28 +200,28 @@ function response(room, msg, sender, isGroupChat, replier) {
 function procCmd(cmd, room, isGroupChat, r, sender) {
 	switch (cmd) {
 		case "/on":
-			Hibot.say("Hibot이 자유가 되었습니다.", r);
+			Hina.say("Hina이 자유가 되었습니다.", r);
 			botOn[room] = true;
 			break;
 		case "/off":
-			if(sender == "Hibot"){
-				Hibot.say("Hibot이 봉인되었습니다", r);
+			if(sender == "Hina"){
+				Hina.say("Hina이 봉인되었습니다", r);
 				botOn[room] = false;
 			}
 			else
-				Hibot.say("嫌い！");
+				Hina.say("嫌い！");
 			break;
-		case "/hibot":
-			Hibot.say("봇 이름: Hibot\n버전: 3.1\n원작자: Dark Tornado\n봇주: 비밀\n\n사람처럼 대화하기 싫은 봇\nFork From Project M - Moka(모카)", r);
+		case "/Hina":
+			Hina.say("봇 이름: Hina\n버전: 3.1\n원작자: Dark Tornado\n봇주: 비밀\n\n사람처럼 대화하기 싫은 봇\nFork From Project M - Moka(모카)", r);
 			break;
 		case "/help":
-			Hibot.say("Hibot의 명령어 목록입니다.\n /on - Hibot을 활성화시킵니다.\n /off - Hibot을 비활성화시킵니다.\n/hibot - Hibot의 정보를 띄웁니다.\n/help - 설마 모르겠어?\n/DB - 해당 채팅방에서 배운 채팅들의 수 출력", r);
+			Hina.say("Hina의 명령어 목록입니다.\n /on - Hina을 활성화시킵니다.\n /off - Hina을 비활성화시킵니다.\n/Hina - Hina의 정보를 띄웁니다.\n/help - 설마 모르겠어?\n/DB - 해당 채팅방에서 배운 채팅들의 수 출력", r);
 			break;
 		case "/DB":
 			if (isGroupChat) {
 				var data = DB.readData(room);
-				if (data == null) Hibot.say("0개", r);
-				else Hibot.say(data.split("\n").length + "개", r);
+				if (data == null) Hina.say("0개", r);
+				else Hina.say(data.split("\n").length + "개", r);
 			}
 			break;
 	}
